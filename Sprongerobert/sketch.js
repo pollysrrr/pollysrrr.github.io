a5136163c41b90f19aa8bdb9b76a79f26e4e2a42
pollysrrr <129110589+pollysrrr@users.noreply.github.com>
2023-05-01 13:16:04 -0400

    Add files via upload

---
+++ Sprongerobert/sketch.js
@@ -0,0 +1,164 @@
+function setup() {
+  createCanvas(400, 400);
+  
+
+}
+
+function draw() {
+ background(173,216,230);
+  fill(65,105,225)//
+  rect(200,200,500,500);//floor
+  rect(0,200,500,500);//floor
+  fill(255,127,57);//orange
+  ellipse(50,150,100,150);//pineapple
+  fill(101,120,149);//grey
+  rect(150,50,100,150)//sqid's house
+  rect(130,100,20,50)//sqid's house
+  rect(250,100,20,50)//sqid's house
+  fill(116,130,142)// window color
+  ellipse(170,120,30,30)
+   fill(255,192,203)//pink
+   triangle(100,500,500,500,200,100)//pat's head
+  fill(255,0,0)
+  ellipse(130,230,20,90)//Mr. Kraps
+  ellipse(160,230,20,90)////mr. kraps
+  ellipse(150,310,70,70)//mr kraps head
+  ellipse(150,390,100,100)
+  fill(255)//white
+  ellipse(200,150,40,40)//pat's eye
+  ellipse(250,150,40,40)//Pat's eye
+ellipse(130,230,10,80)//mr kraps eye
+  ellipse(160,230,10,80)//mr kraps eyes
+  fill(0)//black
+  ellipse(200,150, 20,20)// pat's eye black
+  ellipse(250,150,20,20)// pat's eye black
+  ellipse(130,230,10,10)//kraps eye
+  ellipse(160,230,10,10)//kraps eye
+  fill(156,34,93)
+  ellipse(220,215,50,50)//pat's mouth
+  fill(230,230,0);//yellow
+  rect(150,250,100,130);//face(robert)
+  ellipse(200,310,20,10)//nose (robert)
+  arc(197, 320 , 30,30 , 0,PI, OPEN);//plankturd smile
+  fill(255,255,255);//white
+  ellipse(179,290,40,40);//eye
+  ellipse(220,290,40,40);//eye
+  fill(255,255,255);//white
+  rect(188,330,10,15);//teeth
+  rect(199.,330,10,15);//teeth
+  rect(217,192,15,20)
+  fill(150,75,0);//brown
+  rect(150,350,100,150);//pants
+   fill(0)//black
+  rect(170,260,5,12);//eyelash
+   rect(180,260,5,12);//eyelash
+   rect(160,260,5,12);//eyelash
+   rect(230,260,5,12);//eyelash
+   rect(210,260,5,12);//eyelash
+   rect(220,260,5,12);//eyelash
+  rect(150,350,30,20);//belt
+  rect(220,350,30,20);//belt
+  fill(255,0,0)//red
+  rect(189,350,20,40);//tie
+  fill(225,246,255);//light blue
+  ellipse(180,290,20,20);//iris
+  ellipse(220,290,20,20);//iris
+  fill(0)// black
+  ellipse(180,290,10,10)//pupil
+  ellipse(220,290,10,10)//pupil
+  fill(255)
+  ellipse(70,310,150,150)
+  fill(213,100,124);//pink
+  ellipse(170,310,30,20);//robert's cheek
+  ellipse(230,310,30,20);//robert's cheek
+  ellipse(260,190,30,20)//pat's cheek
+  ellipse(180,190,30,20)//pat's cheek
+  fill(217,230,80)//yellow green
+  ellipse(170,330,8,7)//dots
+   ellipse(230,329,10,8)//dots
+  ellipse(230,329,10,8)//dots
+   ellipse(242,270,12,8)//dots
+  triangle(50,75,70,30,40,75)//leaf
+   triangle(50,75,90,30,40,75)//leaf
+   triangle(50,75,30,30,40,75)//leaf
+     triangle(50,75,100,30,40,75)//leaf
+   triangle(50,75,120,30,40,75)//leaf
+ triangle(50,75,20,30,40,75)//leaf
+  fill(135,206,235)//blue
+  ellipse(20,150,30,30)//window
+  ellipse(80,150,30,30)//window
+square(20, 170, 55, 25);//door
+  fill(11,11,70)//navy blue
+  ellipse(20,150,15,15)//window
+  ellipse(80,150,15,15)//window
+  square(24,175, 45,20)//door
+ fill(128,96,77)//brown
+  arc(350, 230, 100, 100, PI, TWO_PI);//patrock's house
+  fill(190,190,230)//grey
+  rect(300,345,40,50)//head sqid
+  ellipse(320,300,130,100)//head sqid
+  ellipse(320,345,30,70)//nose sqid
+  arc(320 ,380, 20,20 , 0,PI, OPEN);
+  fill(255)//white
+  ellipse(350,295,40,40)//eye sqid
+  ellipse(295,295,40,40)//eyesqid
+  fill(0)//black
+  ellipse(295,295,15,15)//pupils sqid
+  ellipse(350,295,15,15)//pupils sqid
+  fill(188,158,130)//brown
+  ellipse(70,320,100,100)//sandra head
+  ellipse(30,270,30,30)//sandra ears
+  ellipse(110,270,30,30)//sandra ears
+  rect(55,370,30,50)//sandra neck
+  fill(255)//white
+  ellipse(40,310,30,30)//sandra eyes
+  ellipse(100,310,30,30)//sandra eyes
+  fill(188,158,130)
+  arc(70, 339, 30,30, 0,PI, OPEN);//sandra smile
+  fill(255)//white
+  rect(60,350,10,15)//sandra teeth
+  rect(70,350,10,15)//sandra teeth
+  fill(0)//black
+  ellipse(40,310,20,20)//pupils sandra
+  ellipse(100,310,20,20)//pupils sandra
+  fill(255,182,199)//pink
+  ellipse(70,330,20,10)//sandra nose
+  fill(255,0,255)//pink
+  ellipse(40,280,10,10)//flower
+  ellipse(50,270,10,10)//flower
+  ellipse(40,268,10,10)//flower
+  ellipse(30,275,10,10)//flower
+  fill(85,107,47)
+  ellipse(270,370,30,50)//plankturd
+  rect(275,335,5,15)//plankturd antenae
+  rect(260,335,5,15)//plankturd antenae
+   fill(255)
+  ellipse(270,360,20,20)
+  fill(0)
+  ellipse(270,360,10,10)
+  arc()
+  fill(85,107,47)
+   arc(270,380 , 10,10 , 0,PI, OPEN);//plankturd smile
+  fill(255,0,255)
+  ellipse(360,40,30,30)
+  ellipse(340,60,30,30)
+  ellipse(360,80,30,30)
+  ellipse(380,60,30,30)
+  fill(80,220,250)
+  ellipse(360,60,20,20)
+  
+  
+  
+  
+  
+  
+  //spongerobert rectangular slacks, patrock sun, sqidjerk tennisballs, and sandra peaks.from peanut.
+  //non copyright version of spongebob square pants
+  
+
+  
+  
+  
+}
+
+      
